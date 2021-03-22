c73a0343f274b0f90f728a505ca3fa9c89ad8ceb
tsheringpenjor <tpenjor66.tijtech@gmail.com>
2021-03-23 00:47:05 +0600

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,560 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>NyoTsong-Market of Possibilities</title>
+
+    <link rel="stylesheet" type="text/css" href="<?php echo url('/'); ?>/assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/fonts/line-icons.css">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo url('/'); ?>/assets/css/slicknav.css">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo url('/'); ?>/assets/css/color-switcher.css">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo url('/'); ?>/assets/css/animate.css">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo url('/'); ?>/assets/css/owl.carousel.css">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo url('/'); ?>/assets/css/main.css">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo url('/'); ?>/assets/css/responsive.css">
+</head>
+<body>
+
+<header id="header-wrap">
+
+<div class="top-bar">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-5 col-md-7 col-xs-12">
+            <div class="header-top-right float-right">
+                <a href="newlogin" class="header-top-button"><i class="lni-lock"></i> Log In</a> |
+                <a href="newregister" class="header-top-button"><i class="lni-pencil"></i> Register</a>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<nav class="navbar navbar-expand-lg bg-white fixed-top scrolling-navbar">
+<div class="container">
+
+<div class="navbar-header">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    <span class="lni-menu"></span>
+    <span class="lni-menu"></span>
+    <span class="lni-menu"></span>
+</button>
+<a href="index" class="navbar-brand"><img src="assets/img/logo.png" alt=""></a>
+</div>
+<div class="collapse navbar-collapse" id="main-navbar">
+<ul class="navbar-nav mr-auto w-100 justify-content-center">
+<li class="nav-item dropdown active">
+<a class="nav-link dropdown-toggle" href="index" aria-haspopup="true" aria-expanded="false">
+Home
+</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="category">
+Categories
+</a>
+</li>
+ <li class="nav-item dropdown">
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="about" aria-haspopup="true" aria-expanded="false">
+About
+</a>
+<div class="dropdown-menu">
+<a class="dropdown-item" href="faq">FAQ</a>
+</div>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="contact">
+Contact
+</a>
+</li>
+</ul>
+<div class="post-btn">
+<a class="btn btn-common" href="post-ads"><i class="lni-pencil-alt"></i> Post an Ad</a>
+</div>
+</div>
+</div>
+
+<ul class="mobile-menu">
+<li>
+<a class="active" href="#">
+Home
+</a>
+</li>
+<li>
+<li>
+<li>
+<a href="#">About</a>
+<ul class="dropdown">
+<li><a href="faq">FAQ</a></li>
+</ul>
+</li>
+<li>
+<a href="contact">Contact Us</a>
+</li>
+</ul>
+</nav>
+
+
+<div id="hero-area">
+<div class="overlay"></div>
+<div class="container">
+<div class="row justify-content-center">
+<div class="col-md-12 col-lg-9 col-xs-12 text-center">
+<div class="contents">
+<h1 class="head-title">Welcome to Bhutan's Largest Multiseller <span class="year">Marketplace</span></h1>
+<p>Buy and sell everything from used cars to mobile phones and computers, or search lands to buy</p>
+<div class="search-bar">
+<div class="search-inner">
+<form class="search-form">
+<div class="form-group">
+<input type="text" name="customword" class="form-control" placeholder="What are you looking for?">
+</div>
+<div class="form-group inputwithicon">
+<div class="select">
+<select>
+<option value="none">Thimphu</option>
+<option value="none">Paro</option>
+<option value="none">Punakha</option>
+<option value="none">Tashigang</option>
+<option value="none">Sarpang</option>
+<option value="none">Phuntsholing</option>
+<option value="none">Wangdue</option>
+</select>
+</div>
+<i class="lni-target"></i>
+</div>
+<div class="form-group inputwithicon">
+<div class="select">
+<select>
+<option value="none">Select Catagory</option>
+<option value="none"></option>
+<option value="none">Electronics</option>
+<option value="none">Land</option>
+<option value="none">Vehicles</option>
+</select>
+</div>
+<i class="lni-menu"></i>
+</div>
+<button class="btn btn-common" type="button"><i class="lni-search"></i> Search Now</button>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</header>
+
+
+<section id="categories">
+<div class="container">
+<div class="row justify-content-center">
+<div class="col-lg-10 col-md-12 col-xs-12">
+<div id="categories-icon-slider" class="categories-wrapper owl-carousel owl-theme">
+<div class="item">
+<a href="category">
+<div class="category-icon-item">
+<div class="icon-box">
+<div class="icon">
+<img src="assets/img/category/img-1.png" alt="">
+</div>
+<h4>Vehicle</h4>
+</div>
+</div>
+</a>
+</div>
+<div class="item">
+<a href="category">
+ <div class="category-icon-item">
+<div class="icon-box">
+<div class="icon">
+<img src="assets/img/category/img-2.png" alt="">
+</div>
+<h4>Land</h4>
+</div>
+</div>
+</a>
+</div>
+<div class="item">
+<a href="category">
+<div class="category-icon-item">
+<div class="icon-box">
+<div class="icon">
+<img src="assets/img/category/img-3.png" alt="">
+</div>
+<h4>Electronics</h4>
+</div>
+</div>
+</a>
+</div>
+<div class="item">
+<a href="category">
+<div class="category-icon-item">
+<div class="icon-box">
+<div class="icon">
+<img src="assets/img/category/img-1.png" alt="">
+</div>
+<h4>Vehicle</h4>
+</div>
+</div>
+</a>
+</div>
+<div class="item">
+<a href="category">
+<div class="category-icon-item">
+<div class="icon-box">
+<div class="icon">
+<img src="assets/img/category/img-2.png" alt="">
+</div>
+<h4>Land</h4>
+</div>
+</div>
+</a>
+</div>
+<div class="item">
+<a href="category">
+<div class="category-icon-item">
+<div class="icon-box">
+<div class="icon">
+<img src="assets/img/category/img-3.png" alt="">
+</div>
+<h4>Electronics</h4>
+</div>
+</div>
+</a>
+</div>
+<div class="item">
+<a href="category">
+<div class="category-icon-item">
+<div class="icon-box">
+<div class="icon">
+<img src="assets/img/category/img-1.png" alt="">
+</div>
+<h4>Vehicle</h4>
+</div>
+</div>
+</a>
+</div>
+<div class="item">
+<a href="category">
+<div class="category-icon-item">
+<div class="icon-box">
+<div class="icon">
+<img src="assets/img/category/img-2.png" alt="">
+</div>
+<h4>Land</h4>
+</div>
+</div>
+</a>
+</div>
+<div class="item">
+<a href="category">
+<div class="category-icon-item">
+<div class="icon-box">
+<div class="icon">
+<img src="assets/img/category/img-3.png" alt="">
+</div>
+<h4>Electronics</h4>
+</div>
+</div>
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="featured section-padding">
+<div class="container">
+<h1 class="section-title">Products</h1>
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-4">
+<div class="featured-box">
+<figure>
+<a href="#"><img class="img-fluid" src="assets/img/featured/img-1.jpg" alt=""></a>
+</figure>
+<div class="feature-content">
+<div class="product">
+<a href="#">Electronic > </a>
+<a href="#">Phone</a>
+</div>
+<h4><a href="ads-details">iPhone</a></h4>
+<div class="meta-tag">
+<span>
+<a href="#"><i class="lni-user"></i> Bir Bdr Rai</a>
+</span>
+<span>
+<a href="#"><i class="lni-map-marker"></i> Thimphu</a>
+</span>
+<span>
+<a href="#"><i class="lni-tag"></i> iPhone</a>
+</span>
+</div>
+<p class="dsc">The phone is in excellent condition with usage just over a year. Free original airpods as well.</p>
+<div class="listing-bottom">
+<h3 class="price float-left">Nu. 55, 000</h3>
+<a href="ads-details" class="btn btn-common float-right">View Details</a>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-4">
+<div class="featured-box">
+<a href="#"><img class="img-fluid" src="assets/img/featured/img-2.jpg" alt=""></a>
+</figure>
+<div class="feature-content">
+<div class="product">
+<a href="#">Electronic > </a>
+<a href="#">iPhone</a>
+</div>
+<h4><a href="ads-details">iPhone 11 Pro</a></h4>
+<div class="meta-tag">
+<span>
+<a href="#"><i class="lni-user"></i> Bir Bdr Rai</a>
+</span>
+<span>
+<a href="#"><i class="lni-map-marker"></i>Thimphu</a>
+</span>
+<span>
+<a href="#"><i class="lni-tag"></i> Phones</a>
+</span>
+</div>
+<p class="dsc">The phone is in excellent condition with usage just over a year. Free original airpods as well</p>
+<div class="listing-bottom">
+<h3 class="price float-left">Nu. 55, 000</h3>
+<a href="ads-details" class="btn btn-common float-right">View Details</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-4">
+<div class="featured-box">
+<figure>
+<div class="icon">
+</div>
+<a href="#"><img class="img-fluid" src="assets/img/featured/img-3.jpg" alt=""></a>
+</figure>
+<div class="feature-content">
+<div class="product">
+<a href="#">Electronics </a>
+<a href="#">Phone</a>
+</div>
+<h4><a href="ads-details">iPhone 11 Pro</a></h4>
+<div class="meta-tag">
+<span>
+<a href="#"><i class="lni-user"></i> Bir Bdr Rai</a>
+</span>
+<span>
+<a href="#"><i class="lni-map-marker"></i> Thimphu</a>
+</span>
+<span>
+<a href="#"><i class="lni-tag"></i> iPhone</a>
+</span>
+</div>
+<p class="dsc">The phone is in excellent condition with usage just over a year. Free original airpods as well</p>
+<div class="listing-bottom">
+<h3 class="price float-left">Nu. 55, 000</h3>
+<a href="ads-details" class="btn btn-common float-right">View Details</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-4">
+<div class="featured-box">
+<figure>
+<a href="#"><img class="img-fluid" src="assets/img/featured/img-4.jpg" alt=""></a>
+</figure>
+<div class="feature-content">
+<div class="product">
+<a href="#">Electronics </a>
+<a href="#">Phone</a>
+</div>
+<h4><a href="ads-details">iPhone 11 Pro</a></h4>
+<div class="meta-tag">
+<span>
+<a href="#"><i class="lni-user"></i> Bir Bdr Rai</a>
+</span>
+<span>
+<a href="#"><i class="lni-map-marker"></i> Thimphu</a>
+</span>
+<span>
+<a href="#"><i class="lni-tag"></i>iPhone</a>
+</span>
+</div>
+<p class="dsc">The phone is in excellent condition with usage just over a year. Free original airpods as well</p>
+<div class="listing-bottom">
+<h3 class="price float-left">Nu. 55, 000</h3>
+<a href="ads-details" class="btn btn-common float-right">View Details</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-4">
+<div class="featured-box">
+<figure>
+<a href="#"><img class="img-fluid" src="assets/img/featured/img-5.jpg" alt=""></a>
+</figure>
+<div class="feature-content">
+<div class="product">
+<a href="#">Electronic > </a>
+<a href="#">Apple</a>
+</div>
+<h4><a href="ads-details">iPhone 11 Pro</a></h4>
+<div class="meta-tag">
+<span>
+<a href="#"><i class="lni-user"></i> Bir Bdr Rai</a>
+</span>
+<span>
+<a href="#"><i class="lni-map-marker"></i> Thimphu</a>
+</span>
+<span>
+<a href="#"><i class="lni-tag"></i> Apple</a>
+</span>
+</div>
+<p class="dsc">The phone is in excellent condition with usage just over a year. Free original airpods as well</p>
+<div class="listing-bottom">
+<h3 class="price float-left">Nu. 55, 000</h3>
+<a href="ads-details" class="btn btn-common float-right">View Details</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-4">
+<div class="featured-box">
+<figure>
+<a href="#"><img class="img-fluid" src="assets/img/featured/img-6.jpg" alt=""></a>
+</figure>
+<div class="feature-content">
+<div class="product">
+<a href="#">Electronics </a>
+<a href="#">iPhone</a>
+</div>
+<h4><a href="ads-details">iPhone 11 Pro</a></h4>
+<div class="meta-tag">
+<span>
+<a href="#"><i class="lni-user"></i> Bir Bdr Rai</a>
+</span>
+<span>
+<a href="#"><i class="lni-map-marker"></i>Thimphu</a>
+</span>
+<span>
+<a href="#"><i class="lni-tag"></i> Apple</a>
+</span>
+</div>
+<p class="dsc">The phone is in excellent condition with usage just over a year. Free original airpods as well</p>
+<div class="listing-bottom">
+<h3 class="price float-left">Nu. 55, 00</h3>
+<a href="ads-details" class="btn btn-common float-right">View Details</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+
+
+<section class="works section-padding">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<h3 class="section-title">How NyoTsong Works</h3>
+</div>
+<div class="col-lg-4 col-md-4 col-xs-12">
+<div class="works-item">
+<div class="icon-box">
+<i class="lni-users"></i>
+</div>
+<p>Create an Account</p>
+</div>
+</div>
+<div class="col-lg-4 col-md-4 col-xs-12">
+<div class="works-item">
+<div class="icon-box">
+<i class="lni-bookmark-alt"></i>
+</div>
+<p>Post Free Ad</p>
+</div>
+</div>
+<div class="col-lg-4 col-md-4 col-xs-12">
+<div class="works-item">
+<div class="icon-box">
+<i class="lni-thumbs-up"></i>
+</div>
+<p>Deal Done</p>
+</div>
+</div>
+<hr class="works-line">
+</div>
+</div>
+</section>
+
+
+
+
+
+<footer>
+<div id="copyright">
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="site-info text-center">
+<p><a target="_blank" href="#">&copy; NyoTsong 2021</a></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</footer>
+
+
+<a href="#" class="back-to-top">
+<i class="lni-chevron-up"></i>
+</a>
+
+<div id="preloader">
+<div class="loader" id="loader-1"></div>
+</div>
+
+
+<script data-cfasync="false" src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="<?php echo url('/'); ?>/assets/js/jquery-min.js"></script>
+<script src="<?php echo url('/'); ?>/assets/js/popper.min.js"></script>
+<script src="<?php echo url('/'); ?>/assets/js/bootstrap.min.js"></script>
+<script src="<?php echo url('/'); ?>/assets/js/color-switcher.js"></script>
+<script src="<?php echo url('/'); ?>/assets/js/jquery.counterup.min.js"></script>
+<script src="<?php echo url('/'); ?>/assets/js/waypoints.min.js"></script>
+<script src="<?php echo url('/'); ?>/assets/js/wow.js"></script>
+<script src="<?php echo url('/'); ?>/assets/js/owl.carousel.min.js"></script>
+<script src="<?php echo url('/'); ?>/assets/js/jquery.slicknav.js"></script>
+<script src="<?php echo url('/'); ?>/assets/js/main.js"></script>
+<script src="<?php echo url('/'); ?>/assets/js/form-validator.min.js"></script>
+<script src="<?php echo url('/'); ?>/assets/js/contact-form-script.min.js"></script>
+<script src="<?php echo url('/'); ?>/assets/js/summernote.js"></script>
+</body>
+
+</html>
+
+
